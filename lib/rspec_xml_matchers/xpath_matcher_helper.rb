cfef61e92047a8f290dfedac3d0fa5ffd3ef0eaa
Michael Frayer <mrfrayer@yahoo.com>
2012-01-22 08:02:10 -0500

    Refactoring and additional comments in XPathMatcherHelper

--- lib/rspec_xml_matchers/xpath_matcher_helper.rb
+++ lib/rspec_xml_matchers/xpath_matcher_helper.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 
 module RSpecXmlMatchers
+
   class XPathMatcherHelper
     attr_reader :text_value_found
     
@@ -15,18 +16,31 @@ module RSpecXmlMatchers
       end
     end
     
+    # Checks for the existence of the XPath expression in the XML given in
+    # the initializer. If a block is given to this method, it is passed
+    # the Nokogiri::XML::NodeSet result of the XPath search.
     def contains_xpath?
-      xpath_result = @xml_document.xpath(@xpath, @namespaces)
-      block_result = !xpath_result.empty? && block_given? ? yield(xpath_result) : true
-      !xpath_result.empty? && block_result
+      xpath_result_set = @xml_document.xpath(@xpath, @namespaces)
+      yield(xpath_result_set) if block_given?
+      !xpath_result_set.empty?
     end
     
+    # Checks for the existence of the XPath expression in the XML given in
+    # the initializer as well as if the Node returned by that XPath expression
+    # contains the text value given in this method.
     def contains_xpath_with_text_value?(text_value)
-      contains_xpath? do |xpath_result|
-        child = xpath_result.first.child
-        @text_value_found = child.to_s
-        child.text? && (@text_value_found == text_value)
+      contains_text_value = false
+      
+      contains_xpath? do |xpath_result_set|
+        if !xpath_result_set.empty?
+          child = xpath_result_set.first.child
+          contains_text_value = !child.nil? && child.text? && (child.to_s == text_value)
+        end
       end
+      
+      contains_text_value
     end
+    
   end
+
 end
